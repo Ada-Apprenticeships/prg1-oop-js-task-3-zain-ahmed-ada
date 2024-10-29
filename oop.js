31b6619412b11ff8668d02421eea8673360d76a0
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-10-29 15:25:06 +0000

    Passed all test cases

--- oop.js
+++ oop.js
@@ -9,16 +9,14 @@ function validInteger(value) {
 }
 
 function validatePriority(priority) {
-	// Convert string numbers to actual numbers if they are valid integers
 	if (typeof priority === "string" && !isNaN(priority)) {
 		priority = parseInt(priority, 10);
 	}
 
-	// Check for valid priority values
 	if (priority === 1 || priority === 3 || priority === 5 || priority === 7) {
 		return priority;
 	} else {
-		return 1; // Default to LOW priority for invalid inputs
+		return 1;
 	}
 }
 
@@ -35,48 +33,13 @@ function todaysDate() {
 	return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
 
-class ToDo {
-	constructor() {
-		this.tasks = [];
-	}
-
-	add(task) {
-		this.tasks.push(task);
-		return this.tasks.length;
-	}
-
-	remove(title) {
-		for (let i = 0; i < this.tasks.length; i++) {
-			if (title === this.tasks[i].title) {
-				this.tasks.slice(i, 1);
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-
-	list(priority = 0) {
-		for (let i = 0; i < this.tasks.length; i++) {
-			return `${this.tasks[i].added} ${this.tasks[i].title} ${this.tasks[i].priority},`;
-		}
-	}
-
-	task(title) {
-		for (let i = 0; i < this.tasks.length; i++) {
-			return this.tasks[i].title;
-		}
-	}
-}
-
-class Task extends ToDo {
+class Task {
 	_added;
 	_title;
 	_priority;
 
-	constructor(added = todaysDate(), title, priority) {
-		super();
-		this._added = added;
+	constructor(title, priority) {
+		this._added = todaysDate();
 		this._title = title;
 		this._priority = validatePriority(priority);
 	}
@@ -98,18 +61,37 @@ class Task extends ToDo {
 	}
 }
 
-task = new Task(undefined, "Get Cappuccino", PRIORITY["MEDIUM"]);
-console.log(task);
+class ToDo {
+	constructor() {
+		this.tasks = [];
+	}
 
-// Task Example
-// task = new Task("Get Cappuccino", PRIORITY["MEDIUM"]); // Creates an instance of a Task (named task)
-// task.added = "30/4/2023 12:26:26"; // Checking the 'added' attribute of a Task instance returns the date/time it was added.
-// task.title = "Get Cappuccino"; // Checking the 'title' attribute for a Task instance returns the title of the task.'
-// task.priority = 3; // Checking the 'priority' attribute for a Task instance returns an integer 3 (Remember MEDIUM == 3).
-// task.priority = PRIORITY["URGENT"]; // Setting the 'priority' attribute for a Task instance to URGENT (Remember URGENT == 7).
-// task.priority = 7; // Checking the 'priority' attribute for a Task instance returns an integer 7 (Remember URGENT == 7).
-// task.priority = "10"; // Setting the 'priority' attribute for a Task instance to the string '10' (an invalid priority).
-// task.priority = 1; // Checking the 'priority attribute for a Task instance returns an integer 1 (because '10' was an invalid priority so it defaults to 1).
+	add(task) {
+		this.tasks.push(task);
+		return this.tasks.length;
+	}
+
+	remove(title) {
+		const index = this.tasks.findIndex((task) => task.title.toLowerCase() === title.toLowerCase());
+		if (index !== -1) {
+			this.tasks.splice(index, 1);
+			return true;
+		}
+		return false;
+	}
+
+	list(priority = 0) {
+		return this.tasks
+			.filter((task) => priority === 0 || task.priority === priority)
+			.map((task) => [task.added, task.title, task.priority]);
+	}
+
+	task(title) {
+		const foundTask = this.tasks.find((task) => task.title.toLowerCase() === title.toLowerCase());
+		if (!foundTask) throw new Error(`Task '${title}' Not Found`);
+		return foundTask;
+	}
+}
 
 // Leave this code here for the automated tests
 module.exports = {
